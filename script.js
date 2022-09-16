08acdb32aa6825afdca544b47ca124223a41ccad
Artur Marbach <Marbach.a@gmx.net>
2022-09-16 13:13:48 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,44 @@
+let posts=[
+{
+    'author' : 'Golden-Nose',
+    'image' : '',
+    'description' : '#Parfum, #HighQuality, #Oil ',
+    'location' : 'Hamburg',
+},
+{
+    'author' : 'Al-Jazeera',
+    'image' : '',
+    'description' : 'Eine Moschee in Arabien um das Jahr 1783',
+    'location' : 'Jemen',
+},
+{
+    'author' : 'Fifa World',
+    'image' : '',
+    'description' : 'Eine Moschee in Arabien um das Jahr 1783',
+    'location' : '',
+},
+{
+    'author' : 'Golden-Nose',
+    'image' : '',
+    'description' : '',
+    'location' : 'Hamburg',
+},
+{
+    'author' : 'Fifa World',
+    'image' : '',
+    'description' : 'Eine Moschee in Arabien um das Jahr 1783',
+    'location' : 'Jemen',
+},
+{
+    'author' : 'Al-Jazeera',
+    'image' : '',
+    'description' : 'Eine Moschee in Arabien um das Jahr 1783',
+    'location' : 'Jemen',
+},
+];
+
+
+let authos= [];
+let images= [];
+let locations= [];
+let headline= [];
